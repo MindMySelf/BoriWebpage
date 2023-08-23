c9260df9c04d71a86b65327de515d2e0de1d8e46
csege <cseloteigergely@gmail.com>
2023-08-23 14:23:57 +0200

    add basic spring boot library to backend folder

---
+++ backend/src/test/java/CsegeProjects/HomePage/HomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package CsegeProjects.HomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
