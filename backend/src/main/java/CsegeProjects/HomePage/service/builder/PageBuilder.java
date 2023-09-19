abb6788e5f6fa26eb1e4eecbfccb5ada37e22b94
csege <cseloteigergely@gmail.com>
2023-09-19 11:25:27 +0200

    add pageBuilder method content to PageBuilder

--- backend/src/main/java/CsegeProjects/HomePage/service/builder/PageBuilder.java
+++ backend/src/main/java/CsegeProjects/HomePage/service/builder/PageBuilder.java
@@ -1,9 +1,12 @@
 package CsegeProjects.HomePage.service.builder;
 
 import CsegeProjects.HomePage.model.page.Page;
+import CsegeProjects.HomePage.model.page.PageDTO;
 import CsegeProjects.HomePage.repositoryDAO.PageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 public class PageBuilder {
@@ -13,7 +16,13 @@ public class PageBuilder {
     public PageBuilder(PageRepository pageRepository) {
         this.pageRepository = pageRepository;
     }
-//    public Page pageBuilder() {
-//        return Page.builder()
-//    }
+    public Page pageBuilder(PageDTO pageDTO) {
+        return Page.builder()
+                .id(UUID.randomUUID())
+                .title(pageDTO.title())
+                .owner(pageDTO.owner())
+                .template(pageDTO.template())
+                .elements(pageDTO.elements())
+                .build();
+    }
 }
