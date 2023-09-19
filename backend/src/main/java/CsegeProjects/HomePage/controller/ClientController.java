35623364bbbf393ba03e6eb41be9ec224de0d39f
csege <cseloteigergely@gmail.com>
2023-09-19 11:48:39 +0200

    add annotations, field, constructor and 3 endpoints to ClientController, refactor ClientService at method parameters, use of ClientBuilder

--- backend/src/main/java/CsegeProjects/HomePage/controller/ClientController.java
+++ backend/src/main/java/CsegeProjects/HomePage/controller/ClientController.java
@@ -1,4 +1,32 @@
 package CsegeProjects.HomePage.controller;
 
+import CsegeProjects.HomePage.model.client.Client;
+import CsegeProjects.HomePage.model.client.ClientDTO;
+import CsegeProjects.HomePage.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/clients")
 public class ClientController {
+    private final ClientService clientService;
+
+    @Autowired
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+    @GetMapping(value = "{id}")
+    public Client getClientByID(@PathVariable("id")UUID id) {
+        return clientService.getClientById(id);
+    }
+    @PostMapping(value = "add")
+    public void createClient(@RequestBody ClientDTO clientDTO) {
+        clientService.addNewClient(clientDTO);
+    }
+    @PutMapping(value = "put")
+    public void updateClient(@RequestBody ClientDTO clientDTO) {
+        clientService.updateClient(clientDTO);
+    }
 }
--- backend/src/main/java/CsegeProjects/HomePage/service/ClientService.java
+++ backend/src/main/java/CsegeProjects/HomePage/service/ClientService.java
@@ -1,7 +1,9 @@
 package CsegeProjects.HomePage.service;
 
 import CsegeProjects.HomePage.model.client.Client;
+import CsegeProjects.HomePage.model.client.ClientDTO;
 import CsegeProjects.HomePage.repositoryDAO.ClientRepository;
+import CsegeProjects.HomePage.service.builder.ClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +12,23 @@ import java.util.UUID;
 @Service
 public class ClientService {
     private final ClientRepository clientRepository;
+    private final ClientBuilder clientBuilder;
 
     @Autowired
-    public ClientService(ClientRepository clientRepository) {
+    public ClientService(ClientRepository clientRepository, ClientBuilder clientBuilder) {
         this.clientRepository = clientRepository;
+        this.clientBuilder = clientBuilder;
     }
 
-    public void addNewClient(Client client) {
-        clientRepository.save(client);
+    public void addNewClient(ClientDTO client) {
+        clientRepository.save(clientBuilder.buildClient(client));
     }
 
     public Client getClientById(UUID id) {
         return clientRepository.getClientById(id);
     }
 
-    public void updateClient(Client updatedClient) {
-        clientRepository.save(updatedClient);
+    public void updateClient(ClientDTO updatedClient) {
+        clientRepository.save(clientBuilder.buildClient(updatedClient));
     }
 }
