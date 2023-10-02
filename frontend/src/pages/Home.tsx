2a73ba452e5ab2d95a9288d6bd28dabf0b153137
csege <cseloteigergely@gmail.com>
2023-10-02 14:40:07 +0200

    add css to App.tsx, remove Container from return; create css file, add nice stypes to Moe component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,33 +1,41 @@
+import { Col, Container, Row } from "react-bootstrap";
+
 export function Home() {
   return (
     <>
-      <div className="container">
-        <h1>Welcome to Page Maker!</h1>
-        <h2>What can you achieve with our service?</h2>
-        <div className="container">
-          <div className="row">
-            <div className="col col-md">
-              <h3>Create your own webpage!</h3>
-              <p>
-                You can create your dream home page in minutes using our service! 
-                Are you starting your carrier and want to have a webpage for it? <br /> 
-                No need to wait long or pay big, we can assist you to get you started <strong>fast</strong> and <strong>easy</strong>! <br />
-                You can create and even update your page to our website, 
-                no need to deal with databases or whatnot, we got you covered every step on the road!
-              </p>
-            </div>
-            <div className="col">
-              <h3>View others pages!</h3>
-              <p>
-                You are in need of a professional help? Do you tired of Google adds and the not-so-reliable 
-                webpages of private professionals? We can help because our website offer many different webpages
-                to browse in with different kind of profiles which you can easily search in and find the best person
-                for the job you need!
-              </p>
-            </div>
-          </div>
+        <h1 className="main_title">Welcome to Page Maker!</h1>
+        <div className="quote-container">
+          "Everyone needs to start somewhere. <br /> We want to make it simple."
         </div>
-      </div>
+      <Container>
+        <h2>What can you achieve with our service?</h2>
+        <Row>
+          <Col>
+            <h3>Create your own webpage!</h3>
+            <p className="bg-warning rounded p-4">
+              You can create your dream home page in minutes using our service!
+              Are you starting your carrier and want to have a webpage for it?{" "}
+              <br />
+              No need to wait long or pay big, we can assist you to get you
+              started <strong>fast</strong> and <strong>easy</strong>! <br />
+              You can create and even update your page to our website, no need
+              to deal with databases or whatnot, we got you covered every step
+              on the road!
+            </p>
+          </Col>
+          <Col>
+            <h3>View others pages!</h3>
+            <p className="bg-warning rounded p-4">
+              You are in need of a professional help? Do you tired of Google
+              adds and the not-so-reliable webpages of private professionals?{" "}
+              <br /> We can help because our website offer many different
+              webpages to browse in with different kind of profiles which you
+              can easily search in and find the best person for the job you
+              need!
+            </p>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
--- frontend/src/utilities/App.tsx
+++ frontend/src/utilities/App.tsx
@@ -1,23 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { Container } from "react-bootstrap";
 import { Home } from "../pages/Home";
 import { ClientProfile } from "../pages/ClientProfile";
 import { Pages } from "../pages/Pages";
 import { Navbar } from "../components/Navbar";
 import { Templates } from "../pages/Templates";
+import "../index.css"
 
 function App() {
   return (
     <>
-      <Navbar />
-      <Container className="mb-4">
+      <Navbar />      
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/client" element={<ClientProfile />} />
           <Route path="/pages" element={<Pages />} />
           <Route path="/templates" element={<Templates />} />
         </Routes>
-      </Container>
     </>
   );
 }
