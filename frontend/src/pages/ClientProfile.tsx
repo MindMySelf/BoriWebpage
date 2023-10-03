a43d80a819f10cf9d5311360124dbd67c0c920a4
csege <cseloteigergely@gmail.com>
2023-10-03 14:29:01 +0200

    remove @Column from relational fields of Client and Page classes, add suitable connection link into application.properties, can connect to database now

--- frontend/src/pages/ClientProfile.tsx
+++ frontend/src/pages/ClientProfile.tsx
@@ -1,3 +1,34 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+import { Spinner } from "react-bootstrap";
+
+async function fetchClientData(id:string) {
+    return await fetch(`/api/v1/clients/${id}`).then((res) => res.json());
+}
+
 export function ClientProfile() {
+    const { id } = useParams();
+    const [client, setClient] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchClientData(id)
+        .then((client) => {
+            setClient(client);
+            setLoading(false);
+        })
+        .catch((err) => {
+            throw err;
+        })
+    }, [id]);
+
+    if(loading) {
+        return (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          );
+    }
+
     return <h1>Here is the User Profile!</h1>
 }
