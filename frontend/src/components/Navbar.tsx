3744a497e4cdad810e781e4205113e8cc7099772
csege <cseloteigergely@gmail.com>
2023-10-04 11:37:58 +0200

    change <a> tags to Links in Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Button, Container, Nav, Navbar as NavbarAs } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
@@ -46,25 +46,25 @@ export function Navbar() {
           </Button>
           <ul className="dropdown-menu">
             <li>
-              <a className="dropdown-item" href="/client">
+              <Link className="dropdown-item" to="/client">
                 Profile
-              </a>
+              </Link>
             </li>
             <li>
-              <a className="dropdown-item" href="/history">
+              <Link className="dropdown-item" to="/history">
                 History
-              </a>
+              </Link>
             </li>
             <li>
-              <a className="dropdown-item" href="/clientpages">
+              <Link className="dropdown-item" to="/clientpages">
                 Pages
-              </a>
+              </Link>
             </li>
             <li>
               <hr className="dropdown-divider" />
             </li>
             <li>
-              <a className="dropdown-item" href="/logout">
+              <Link className="dropdown-item" to="/logout">
                 {/* <svg
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
@@ -73,7 +73,7 @@ export function Navbar() {
                   <path d="M4,12a1,1,0,0,0,1,1h7.59l-2.3,2.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0l4-4a1,1,0,0,0,.21-.33,1,1,0,0,0,0-.76,1,1,0,0,0-.21-.33l-4-4a1,1,0,1,0-1.42,1.42L12.59,11H5A1,1,0,0,0,4,12ZM17,2H7A3,3,0,0,0,4,5V8A1,1,0,0,0,6,8V5A1,1,0,0,1,7,4H17a1,1,0,0,1,1,1V19a1,1,0,0,1-1,1H7a1,1,0,0,1-1-1V16a1,1,0,0,0-2,0v3a3,3,0,0,0,3,3H17a3,3,0,0,0,3-3V5A3,3,0,0,0,17,2Z"></path>
                 </svg> */}
                 Log out
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
